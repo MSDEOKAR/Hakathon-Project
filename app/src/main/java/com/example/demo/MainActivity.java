60f35ba8d95a1feee3cc9bdbba7111230e631a9f
MSDEOKAR <deokarmahesh506@gmail.com>
2023-03-18 12:56:05 -0700

    Final Commit

--- app/src/main/java/com/example/demo/MainActivity.java
+++ app/src/main/java/com/example/demo/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.demo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,52 +17,31 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
-    Button submit;
-    EditText number,msg;
+
+   private EditText editTextNumber,editTextmsg;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        submit=(Button)findViewById(R.id.submit);
-        number=(EditText) findViewById(R.id.number);
-        msg=(EditText)findViewById(R.id.msg);
-        submit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String phone=number.getText().toString();
-                String msgText=msg.getText().toString();
 
-                try {
-                    // Construct data
-                    String apiKey = "apikey=" + "NmQ2YTc5NjM0OTM0NDk2ODc1MzA1NjQ0Nzk0NTM5Mzc=";
-                    String message = "&message=" +msgText;
-                    String sender = "&sender=" + "TXTLCL";
-                    String numbers = "&numbers=" +phone;
-
-                    // Send data
-                    HttpURLConnection conn = (HttpURLConnection) new URL("https://api.textlocal.in/send/?").openConnection();
-                    String data = apiKey + numbers + message + sender;
-                    conn.setDoOutput(true);
-                    conn.setRequestMethod("POST");
-                    conn.setRequestProperty("Content-Length", Integer.toString(data.length()));
-                    conn.getOutputStream().write(data.getBytes("UTF-8"));
-                    final BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                    final StringBuffer stringBuffer = new StringBuffer();
-                    String line;
-                    while ((line = rd.readLine()) != null) {
-                        stringBuffer.append(line);
-                    }
-                    rd.close();
+        ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.SEND_SMS,Manifest.permission.READ_SMS}, PackageManager.PERMISSION_GRANTED);
+        editTextNumber=findViewById(R.id.Number);
+        editTextmsg=findViewById(R.id.msg);
 
 
-                } catch (Exception e) {
-                    System.out.println("Error SMS "+e);
 
-                }
-            }
-        });
+
+    }
+    public void sendSms(View view) {
+        String number = editTextNumber.getText().toString();
+        String message = editTextmsg.getText().toString();
+
+
+        SmsManager mysms = SmsManager.getDefault();
+        mysms.sendTextMessage(number, null, message, null, null);
+
 
     }
 }
